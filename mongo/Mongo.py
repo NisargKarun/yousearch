dfffc9f39174dffa2444ad3883d3e6a6be42ff17
NisargKarun <nisargkarun1997@gmail.com>
2021-05-26 02:01:15 +0530

    Add layer to interact with the mongo db cluster

---
+++ mongo/Mongo.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+import yaml
+import os
+import pathlib
+
+class MongoDB(object):
+    def __init__(self, database_name=None, collection_name=None):
+        with open(str(pathlib.Path(__file__).parent.absolute())+'/credentials.yaml') as f:
+            docs = yaml.load(f)
+        url = docs['client'].format(password=docs['password'],database_name=database_name)
+        try:
+            self._client = MongoClient(url)
+        except Exception as error:
+            raise Exception(error)
+        self._database = self._client[database_name]
+        self._collection = self._database[collection_name]
+
+    def insert(self, post):
+        post_ids = []
+        try:
+            post_ids = self._collection.insert_many(post).inserted_ids
+        except Exception as error:
+            print(Exception(error))
+        return post_ids
+
+    def find(self, query=None):
+        return self._collection.find(query)
+
+
